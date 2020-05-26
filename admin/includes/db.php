b838354ff274e4ce8d9323afa65a714ca806453e
bregzor <50908913+bregzor@users.noreply.github.com>
2020-05-26 11:44:30 +0200

    admin
    admin part

---
+++ admin/includes/db.php
@@ -0,0 +1,15 @@
+<?php
+
+$db_server = "localhost";
+$db_database = "jlwvfkou_wp888";
+$db_username = "root";
+$db_password = "";
+//$db_password = 'JSU8Sp83[!';
+
+$dsn = "mysql:host=$db_server;dbname=$db_database;charset=utf8";
+try {
+  $pdo = new PDO($dsn, $db_username, $db_password);
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+  echo $e->getMessage();
+}
